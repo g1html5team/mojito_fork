391c8b34549e7617045537f9018c5309586a5afe
Anders Holmgren <andersmholmgren@gmail.com>
2014-10-18 19:16:26 +1100

    copyright

--- lib/src/middleware.dart
+++ lib/src/middleware.dart
@@ -1,3 +1,8 @@
+// Copyright (c) 2014, The Mojito project authors.
+// Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by
+// a BSD 2-Clause License that can be found in the LICENSE file.
+
 library mojito.middleware;
 
 import 'package:shelf/shelf.dart';
