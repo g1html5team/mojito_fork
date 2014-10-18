29ca16acb6b9ceb9ac20e164cc1e1df313550f03
Anders Holmgren <andersmholmgren@gmail.com>
2014-10-18 19:19:49 +1100

    test

---
+++ test/all_tests.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2014, The Mojito project authors.
+// Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by
+// a BSD 2-Clause License that can be found in the LICENSE file.
+
+library mojito.all.test;
+
+
+
+main() {
+  // TODO: ;-)
+}
+
