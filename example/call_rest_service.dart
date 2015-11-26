4db7e407a0ce26710a7f3ecb8e2661a69444e5d6
Anders Holmgren <andersmholmgren@gmail.com>
2015-11-27 08:44:40 +1100

    calling web service example

--- example/call_rest_service.dart
+++ example/call_rest_service.dart
@@ -8,30 +8,29 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
+const String nomeAKUrl = 'https://query.yahooapis.com/v1/public/yql?'
+    'q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20'
+    '(select%20woeid%20from%20geo.places(1)%20'
+    'where%20text%3D%22nome%2C%20ak%22)'
+    '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
+const String greenlandUrl = 'https://query.yahooapis.com/v1/public/yql?'
+    'q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20'
+    '(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'
+    'greenland%22)&format=json&'
+    'env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
 main() {
   final app = init(isDevMode: () => true);
 
   app.router
     ..get('weather', () async {
-      final nomeAKUrl = 'https://query.yahooapis.com/v1/public/yql?'
-          'q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20'
-          '(select%20woeid%20from%20geo.places(1)%20'
-          'where%20text%3D%22nome%2C%20ak%22)'
-          '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
-
-      final greenlandUrl = 'https://query.yahooapis.com/v1/public/yql?'
-          'q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20'
-          '(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'
-          'greenland%22)&format=json&'
-          'env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
-
       final nomeFuture = http.get(nomeAKUrl);
       final greenlandFuture = http.get(greenlandUrl);
 
       final result = await Future.wait([nomeFuture, greenlandFuture]);
       final bodies =
           result.map((http.Response r) => JSON.decode(r.body)).toList();
-      print(bodies);
       return {"nome": bodies[0], "greenland": bodies[1]};
     });
 
