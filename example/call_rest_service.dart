f66d58b23045ad710bd757ecc6b00ba4a73dcb48
Anders Holmgren <andersmholmgren@gmail.com>
2015-11-27 17:28:19 +1100

    calling web service example

--- example/call_rest_service.dart
+++ example/call_rest_service.dart
@@ -7,6 +7,8 @@ import 'package:mojito/mojito.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
+import 'package:shelf/shelf.dart';
+import 'package:logging/logging.dart';
 
 const String nomeAKUrl = 'https://query.yahooapis.com/v1/public/yql?'
     'q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20'
@@ -20,6 +22,8 @@ const String greenlandUrl = 'https://query.yahooapis.com/v1/public/yql?'
     'greenland%22)&format=json&'
     'env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
 
+Logger _log = new Logger('example');
+
 main() {
   final app = init(isDevMode: () => true);
 
@@ -32,7 +36,63 @@ main() {
       final bodies =
           result.map((http.Response r) => JSON.decode(r.body)).toList();
       return {"nome": bodies[0], "greenland": bodies[1]};
-    });
+    })
+    ..get('streamed', () {
+      final sc = new StreamController();
+      final nomeFuture = http.get(nomeAKUrl).then((r) {
+        _log.info('nome');
+
+        return sc.add(r.body);
+      });
+      final greenlandFuture = http.get(greenlandUrl).then((r) async {
+        _log.info('greenland');
+        await new Future.delayed(const Duration(seconds: 10));
+        _log.info('after greenland');
+        return sc.add(r.body);
+      });
+
+      _log.info('wait');
+
+      Future.wait([nomeFuture, greenlandFuture]).then((_) {
+        _log.info('close');
+        return sc.close();
+      });
+
+      _log.info('return');
+
+      final os = sc.stream
+//              .map((r) => r.body)
+//              .transform(JSON.encoder)
+          .transform(UTF8.encoder);
+
+      return new Response.ok(os, context: {"shelf.io.buffer_output": false});
+    })
+    ..get('users', getUsersHandler);
 
   app.start();
+}
+
+Response getUsersHandler(Request request) {
+  Stream<List<int>> counterStream = timedCounter(const Duration(seconds: 1), 15)
+      .map((int x) => 'hello $x\n')
+      .transform(UTF8.encoder);
+
+  print(request.headers);
+  return new Response.ok(counterStream,
+      context: {"shelf.io.buffer_output": false});
+}
+
+Stream<int> timedCounter(Duration interval, [int maxCount]) {
+  StreamController<int> controller = new StreamController<int>();
+  int counter = 0;
+  void tick(Timer timer) {
+    counter++;
+    controller.add(counter); // Ask stream to send counter values as event.
+    if (maxCount != null && counter >= maxCount) {
+      timer.cancel();
+      controller.close(); // Ask stream to shut down and tell listeners.
+    }
+  }
+  new Timer.periodic(interval, tick); // BAD: Starts before it has subscribers.
+  return controller.stream;
 }
